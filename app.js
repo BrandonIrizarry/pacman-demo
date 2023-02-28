a4aab983bf4f73ae18b7c11f2f0249ab66f86b18
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-02-27 22:04:19 -0500

    Move pacman with the keyboard

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const pacman = document.querySelector("svg.pacman");
 
-pacman.addEventListener("keydown", (event) => {
+pacman.style.top = "0px";
+pacman.style.left = "0px";
+
+document.addEventListener("keydown", (event) => {
     switch (event.key) {
     case "ArrowUp":
-        pacman.style.top = pacman.style.top - 1;
+        moveUp();
+        break;
+    case "ArrowDown":
+        moveDown();
+        break;
+    case "ArrowLeft":
+        moveLeft();
+        break;
+    case "ArrowRight":
+        moveRight();
         break;
     }
 });
+
+function moveLeft() {
+    pacman.style.left = parseInt(pacman.style.left) - 5 + "px";
+}
+
+function moveUp() {
+    pacman.style.top = parseInt(pacman.style.top) - 5 + "px";
+}
+
+function moveRight() {
+    pacman.style.left = parseInt(pacman.style.left) + 5 + "px";
+}
+
+function moveDown() {
+    pacman.style.top = parseInt(pacman.style.top) + 5 + "px";
+}
